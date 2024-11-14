02c8ff8dc4569cd4200e74bde9e061c243d6b2ec
codekappal <138691869+codekappal@users.noreply.github.com>
2024-11-14 15:32:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "admin.kappal.in",
+      },
+      {
+        protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
       {
